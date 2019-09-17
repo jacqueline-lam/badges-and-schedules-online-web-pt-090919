751f5c9cdac8a9311faa8bcd6a86ea4f7679e098
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 19:49:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,9 @@ end
 def batch_badge_creator(array_of_names)
   array_of_names.collect {|name| badge_maker(name)}
 end
+
+def assign_rooms(speaker_list)
+  speaker_list.each_with_index do |speaker|
+    "Hello, #{name}! You'll be assigned to room #{room_num}!"
+  end
+end
