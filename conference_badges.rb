e7fbc143c18b6455766f6576f5038fd6b3122014
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 19:55:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator(array_of_names)
-#   array_of_names.collect {|name| badge_maker(name)}
-# end
+def batch_badge_creator(array_of_names)
+  array_of_names.collect {|name| badge_maker(name)}
+end
 
 # def assign_rooms(speaker_list)
 #   speakers_with_rooms = [] 
