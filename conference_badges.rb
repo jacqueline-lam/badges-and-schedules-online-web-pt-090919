9e03d9d4b22673d27848355eccc9bfad0524557b
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 19:54:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,17 +2,18 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_of_names)
-  array_of_names.collect {|name| badge_maker(name)}
-end
+# def batch_badge_creator(array_of_names)
+#   array_of_names.collect {|name| badge_maker(name)}
+# end
 
-def assign_rooms(speaker_list)
-  speakers_with_rooms = [] 
-  speaker_list.each_with_index do |speaker, room|
-    speakers_with_rooms.push("Hello, #{speaker}! You'll be assigned to room #{room+1}!")
-  end
-end
+# def assign_rooms(speaker_list)
+#   speakers_with_rooms = [] 
+#   speaker_list.each_with_index do |speaker, room|
+#     speakers_with_rooms.push("Hello, #{speaker}! You'll be assigned to room #{room+1}!")
+#   end
+# end
 
-def printer(array)
-  batch_badge_creator(array)
-end
+# def printer(array)
+#   batch_badge_creator(array)
+#   puts assign_rooms(speaker_list)
+# end
