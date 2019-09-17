7cf70eb2f70d402237bb2bedc4a08a1dc248e88a
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 19:55:59 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -61,18 +61,18 @@ TEXT
 
   end
 
-  # describe '#batch_badge_creator' do
+  describe '#batch_badge_creator' do
 
-  #   # Question 2b
+    # Question 2b
 
-  #   it 'should return a list of badge messages' do
-  #     expect(batch_badge_creator(attendees)).to eq(badges)
-  #   end
-  #   it 'should not hard-code response' do
-  #     expect(batch_badge_creator(["Johnny"])).to eq(["Hello, my name is Johnny."])
-  #   end
+    it 'should return a list of badge messages' do
+      expect(batch_badge_creator(attendees)).to eq(badges)
+    end
+    it 'should not hard-code response' do
+      expect(batch_badge_creator(["Johnny"])).to eq(["Hello, my name is Johnny."])
+    end
 
-  # end
+  end
 
   # describe '#assign_rooms' do
 
