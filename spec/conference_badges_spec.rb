c7d9e0a469f6461802b8cb13908367916269e464
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-17 19:56:16 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -74,18 +74,18 @@ TEXT
 
   end
 
-  # describe '#assign_rooms' do
+  describe '#assign_rooms' do
 
-  #   # Question 3
+    # Question 3
 
-  #   it 'should return a list of welcome messages and room assignments' do
-  #     expect(assign_rooms(attendees)).to eq(room_assignments)
-  #   end
-  #   it 'should not hard-code the response' do
-  #     expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
-  #   end
+    it 'should return a list of welcome messages and room assignments' do
+      expect(assign_rooms(attendees)).to eq(room_assignments)
+    end
+    it 'should not hard-code the response' do
+      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+    end
 
-  # end
+  end
 
   # describe '#printer' do
 
